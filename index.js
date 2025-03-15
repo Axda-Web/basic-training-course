36b5b26d45bcecda3a313fe9670eaa59f6de8b35
Alyx DARENNE <axda.web@gmail.com>
2025-03-15 15:18:11 +0100

    Ajout affichage dynamique icon bookmark + Mise en place json-server

--- index.js
+++ index.js
@@ -9,19 +9,33 @@ async function fetchMedias() {
     const mediaCard = `<article class="media-card">
                         <div class="media-card__image-container">
                             <img class="media-card__image" src="./Rectangle.jpg" alt="media cover">
-                            <button class="media-card__bookmark-button">
-                                <img class="media-card__bookmark-button__icon" src="./bookmark.svg" alt="bookmark icon">
+                            <button data-id=${
+                              media.id
+                            } class="media-card__bookmark-button">
+                                <img class="media-card__bookmark-button__icon" src=${
+                                  media.isBookmarked
+                                    ? "/bookmark-full.svg"
+                                    : "/bookmark.svg"
+                                } alt="bookmark icon">
                             </button>
                         </div>
                             <div class="media-card__body">
                             <div class="media-card__body__info">
-                                <span class="media-card__body__info__year">${media.year}</span>
+                                <span class="media-card__body__info__year">${
+                                  media.year
+                                }</span>
                                 ·<span class="media-card__body__info__type">
-                                    <img class="media-card__body__info__type__icon" src="${media.image}" alt="movie icon">Movie
+                                    <img class="media-card__body__info__type__icon" src="${
+                                      media.image
+                                    }" alt="movie icon">Movie
                                 </span>
-                                    ·<span class="media-card__body__info__rate">${media.rate}</span>
+                                    ·<span class="media-card__body__info__rate">${
+                                      media.rate
+                                    }</span>
                             </div>
-                            <h3 class="media-card__body__title">${media.title}</h3>
+                            <h3 class="media-card__body__title">${
+                              media.title
+                            }</h3>
                         </div>
                     </article>`;
     mediaContainer.innerHTML += mediaCard;
@@ -33,6 +47,7 @@ async function fetchMedias() {
 
   const toggleBookmark = (event) => {
     const currentSrc = event.currentTarget.children[0].src;
+    const currentDataId = event.currentTarget.dataset.id;
 
     if (currentSrc === "http://127.0.0.1:5500/bookmark.svg") {
       event.currentTarget.children[0].src =
---
+++ note.md
@@ -0,0 +1,12 @@
+## 15/03/25
+
+Installer nodejs
+Lancer la commande: 'npm install'
+
+Utilisation json-server:
+
+- Ouvrir une fenêtre terminal
+- Exécuter la commande: 'npx json-server --watch db.json'
+- Télécharge le plugin Edge JSON Formater: 'https://microsoftedge.microsoft.com/addons/detail/json-formatter-for-edge/njpoigijhgbionbfdbaopheedbpdoddi'
+- Entrer l'URL suivante dans le navigateur pour voir les données server: 'http://localhost:3000/medias'
+- Concept a voir: qu'est ce que le 'CRUD'
